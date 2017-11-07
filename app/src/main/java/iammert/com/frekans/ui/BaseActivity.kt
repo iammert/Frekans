04567ec2ee9836516aab90b446e3536a7c67241e
mertsimsek <mertsimsek1992@gmail.com>
2017-11-07 23:16:02 +0300

    Add required dependencies to gradle.

---
+++ app/src/main/java/iammert/com/frekans/ui/BaseActivity.kt
@@ -0,0 +1,14 @@
+package iammert.com.frekans.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by mertsimsek on 06/11/2017.
+ */
+open class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/iammert/com/frekans/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package iammert.com.frekans.ui
+
+import android.os.Bundle
+
+/**
+ * Created by mertsimsek on 06/11/2017.
+ */
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
